32e2e8b0db1da8cd842146a25770bc7945733bbe
Tassilo von Parseval <vparseval@gmail.com>
2015-08-20 20:28:23 -0400

    move location in front of contact section; make styling of map more standard and reduce padding above

--- js/grayscale.js
+++ js/grayscale.js
@@ -48,28 +48,10 @@ function init() {
         // The latitude and longitude to center the map (always required)
         center: new google.maps.LatLng(40.744737, -73.993539), // New York
 
-
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
-        scrollwheel: true,
+        scrollwheel: false,
         draggable: true,
-
-        // How you would like to style the map. 
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: [{
-           "featureType":"all",
-           "stylers": [
-                {
-                    "hue":"#0000b0"
-                },
-                {
-                    "invert_lightness":"true"
-                },
-                {
-                    "saturation":-30
-                }
-            ]
-        }]
     };
 
     // Get the HTML DOM element that will contain your map 
@@ -79,12 +61,11 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
 
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
+    // Custom Map Marker Icon - Customize the map-marker.png file to customize
+    // your icon
     var myLatLng = new google.maps.LatLng(40.744737, -73.993539);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        icon: image
     });
 }
